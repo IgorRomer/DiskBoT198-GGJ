24c940fe039e0bf4575f1613982d354c22cc1fbf
IgorHomer <igor_romer@hotmail.com>
2020-02-03 13:13:01 -0300

    second

--- Assets/Scripts/godScript.cs
+++ Assets/Scripts/godScript.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class godScript : MonoBehaviour
 {
     public GameObject[] easyList;
-
+    public Text txt;
+    public float timer = 25.198f;
     // Start is called before the first frame update
     void Start()
     {
+        timer = 25.198f;
         Instantiate(easyList[Random.Range(0, 4)], new Vector3(0, 0, 0), Quaternion.identity);
         
     }
@@ -16,6 +20,12 @@ public class godScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        txt.text = "" + timer;
+
+        if(timer <= 0)
+        {
+            SceneManager.LoadScene(4);
+        }
     }
 }
